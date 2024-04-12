e9c06bab6f28c4b3f6e420163dda55c1ac1b23bf
Josh Frankel <joshmfrankel@gmail.com>
2024-04-12 15:41:42 -0400

    Revert "As a teacher i can upload a document to the portal (#5)"
    This reverts commit 802f509f8da9e837100f59b6d9b1a940b663cb00.

--- app/models/document.rb
+++
@@ -1,7 +0,0 @@
-class Document < ApplicationRecord
-  has_one_attached :file
-
-  # @todo
-  # Future
-  # Validations for file size and file type
-end
--- db/migrate/20240410184154_create_documents.rb
+++
@@ -1,9 +0,0 @@
-class CreateDocuments < ActiveRecord::Migration[7.1]
-  def change
-    create_table :documents do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
