7f37c0ccc1f2c6defb16fe35b47b0f6bb0df596c
Josh Frankel <joshmfrankel@gmail.com>
2024-04-13 14:32:55 -0400

    Add public route for displaying accessible documents for students
    * Add view_components for reusable front-end
    * Add heroicons for easy SVG icons
    * Refactor DocumentFileComponent => FilePreviewComponent
    * Store uploader association on Documents
    * Add uuid to Documents in order to provide a unique identifier for public url
    * Rework scaffolded Documents views
    * Fix region for AWS

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,6 @@
 class Document < ApplicationRecord
   has_one_attached :file
+  belongs_to :uploader, class_name: :User
 
   # @todo
   # Future
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'documents/:id', to: "documents#show"
+  end
   resources :documents
   devise_for :users
 
